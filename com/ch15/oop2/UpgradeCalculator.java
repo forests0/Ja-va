ee9e9edb22fba484602e8067a9690eb0225e8a64
Forests <99336909+forests0@users.noreply.github.com>
2022-07-09 09:52:47 +0900

    com ch15 계산기 수정

---
+++ com/ch15/oop2/UpgradeCalculator.java
@@ -0,0 +1,13 @@
+package com.company.example.ch15.oop2;
+
+public class UpgradeCalculator extends Calculator{
+
+    public UpgradeCalculator() {
+        this.value = value;
+    }
+
+    void minus(int val) {
+        this.value -= val;
+    }
+
+}
